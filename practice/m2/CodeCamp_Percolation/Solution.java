a701021a88c8d39570b8f6137a8e0e011981a6b7
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-25 20:09:13 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- practice/m2/CodeCamp_Percolation/Solution.java
+++ practice/m2/CodeCamp_Percolation/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Percolation {
 	private boolean[][] grid;
@@ -10,11 +10,11 @@ class Percolation {
 	//int[] onedim;
 	public Percolation(int gridsize) { // create n-by-n grid, with all sites blocked
 		grid = new boolean[gridsize][gridsize];
-	    unionobj = new WeightedQuickUnionUF(gridsize*gridsize+2);
-	    openblocks = 0;
-	    size = gridsize;
-	    virtualtop = gridsize*gridsize;
-	    virtualbottom = virtualtop+1;
+		unionobj = new WeightedQuickUnionUF(gridsize * gridsize + 2);
+		openblocks = 0;
+		size = gridsize;
+		virtualtop = gridsize * gridsize;
+		virtualbottom = virtualtop + 1;
 
 		// onedim = new int[(n * n) + 2];
 		// onedim[onedim.length-1] = 28;
@@ -27,42 +27,42 @@ class Percolation {
 	}
 
 	public void open(int row, int col) { // open site (row, col) if it is not open already
-        if(grid[row][col] == true){
-        	return;
-        }
+		if (grid[row][col] == true) {
+			return;
+		}
 		grid[row][col] = true;
-		openblocks = openblocks+1;
+		openblocks = openblocks + 1;
 
-        //checking top
-		if(row-1>=0 && isOpen(row-1,col)==true){
-			unionobj.union(size*row+col,size*(row-1)+col);
+		//checking top
+		if (row - 1 >= 0 && isOpen(row - 1, col) == true) {
+			unionobj.union(size * row + col, size * (row - 1) + col);
 		}
 
 		//checking bottom
-		if(row+1<size && isOpen(row+1,col)==true){
-			unionobj.union(size*row+col,size*(row+1)+col);
+		if (row + 1 < size && isOpen(row + 1, col) == true) {
+			unionobj.union(size * row + col, size * (row + 1) + col);
 		}
 
 		//checking left
 
-		if(col-1>=0 && isOpen(row,col-1)==true){
-			unionobj.union(size*row+col,size*row+(col-1));
+		if (col - 1 >= 0 && isOpen(row, col - 1) == true) {
+			unionobj.union(size * row + col, size * row + (col - 1));
 		}
 
 		//checking right
 
-		if(col+1<size && isOpen(row,col+1)==true){
-			unionobj.union(size*row+col,size*row+(col+1));
+		if (col + 1 < size && isOpen(row, col + 1) == true) {
+			unionobj.union(size * row + col, size * row + (col + 1));
 		}
 
-        //top virtual node
-		if(row == 0){
-			unionobj.union(size*row+col,virtualtop);
+		//top virtual node
+		if (row == 0) {
+			unionobj.union(size * row + col, virtualtop);
 		}
 
-        //bottom virtual node
-		if(row == size-1){
-			unionobj.union(size*row+col,virtualbottom);
+		//bottom virtual node
+		if (row == size - 1) {
+			unionobj.union(size * row + col, virtualbottom);
 		}
 
 
@@ -101,10 +101,10 @@ class Percolation {
 	// 	}
 	// }
 
-    public boolean percolate(){
-    	return unionobj.connected(virtualtop,virtualbottom);
+	public boolean percolate() {
+		return unionobj.connected(virtualtop, virtualbottom);
 
-    }
+	}
 }
 
 // You can implement the above API to solve the problem
@@ -119,9 +119,9 @@ class Solution {
 		Percolation obj = new Percolation(order);
 		while (scn.hasNext()) {
 			String[] openvalues = scn.nextLine().split(" ");
-			obj.open((Integer.parseInt(openvalues[0]))-1, (Integer.parseInt(openvalues[1]))-1);
-        }
-        System.out.println(obj.percolate());
+			obj.open((Integer.parseInt(openvalues[0])) - 1, (Integer.parseInt(openvalues[1])) - 1);
+		}
+		System.out.println(obj.percolate());
 
 
 	}
