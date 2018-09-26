971938916edc7a9d809c426a1e22c02d6dfa83a8
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-26 20:09:36 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- practice/m3/3-sum/Solution.java
+++ practice/m3/3-sum/Solution.java
@@ -1,62 +1,74 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
-class Threesum {
-	private int[] array;
-	private int size;
+// class Threesum {
+// 	private int[] array;
+// 	private int size;
 
-	Threesum(int arraysize) {
-		array = new int[arraysize];
-		size = 0;
-	}
+// 	Threesum(int arraysize) {
+// 		array = new int[arraysize];
+// 		size = 0;
+// 	}
 
-	public int[] getarray() {
-		return array;
-	}
+// 	public int[] getarray() {
+// 		return array;
+// 	}
 
-	public void add(int value) {
-		array[size++] = value;
+// 	public void add(int value) {
+// 		array[size++] = value;
 
-	}
+// 	}
 
-	public void sumofthree() {
-		int count = 0;
-		Arrays.sort(array);
-		int sum = 0;
+// 	public void sumofthree() {
+// 		int count = 0;
+// 		Arrays.sort(array);
+// 		int sum = 0;
 
-        //System.out.println(size);
-		for (int i = 0; i < size ; i++) {
+//         //System.out.println(size);
+// 		for (int i = 0; i < size ; i++) {
 
-			for (int j = i + 1; j < size; j++) {
+// 			for (int j = i + 1; j < size; j++) {
 
-				for (int k = j + 1; k < size; k++) {
-					sum = array[i] + array[j] + array[k];
-					if (sum == 0) {
-						count = count + 1;
-					}
-				}
-			}
-		}
-		System.out.println(count);
-	}
-
-
+// 				for (int k = j + 1; k < size; k++) {
+// 						sum = array[i] + array[j] + array[k];
+// 					if (sum == 0) {
+// 						count = count + 1;
+// 					}
+// 				}
+// 			}
+// 		}
+// 		System.out.println(count);
+// 	}
 
 
 
 
-}
+
+
+// }
 class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int arraysize = scan.nextInt();
-		Threesum object1 = new Threesum(arraysize);
+		int count = 0;
+		//Threesum object1 = new Threesum(arraysize);
+		int array[] = new int[arraysize];
 		for (int i = 0; i < arraysize ; i++) {
-			object1.add(scan.nextInt());
+			//object1.add(scan.nextInt());
+			array[i] = scan.nextInt();
 		}
+		Arrays.sort(array);
+		for(int i= 0;i<arraysize;i++){
+			for(int j = i+1;j<arraysize ;j++){
+				int k = Arrays.binarySearch(array, -(array[i]+array[j]));
+				if(k>j)count=count+1;
+			}
+		}
+		System.out.println(count);
+
 
 		//System.out.println(Arrays.toString(object1.getarray()));
-		object1.sumofthree();
+		//object1.sumofthree();
 
 
 	}
