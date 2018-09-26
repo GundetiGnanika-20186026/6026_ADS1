6aecef1d4bd64ee68e9e5e19b3245fcab3863595
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-27 00:37:15 +0530

    "check_style_score -> 0 of 4 passed. style score: 1/1 "

--- practice/m3/3-sum/Solution.java
+++ practice/m3/3-sum/Solution.java
@@ -81,7 +81,7 @@ final class Solution {
     //sending the values to the binary search and
     // searching wether it is present.
 int k = Arrays.binarySearch(array, -(array[i] + array[j]));
-                if (k > j) {
+                if (k >= 0) {
                     count = count + 1;
                 }
             }
