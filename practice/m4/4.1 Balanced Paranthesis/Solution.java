d906e37ce4051c38066b9a26136e73fc9c7ed0e2
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-27 16:39:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,34 +1,33 @@
 import java.util.Scanner;
 import java.util.Stack;
-class Solution{
+class Solution {
 	public static void main(String[] args) {
 		Stack<String> mystack = new Stack<String>();
 		Scanner scan = new Scanner(System.in);
 		int inputcount = Integer.parseInt(scan.nextLine());
-        boolean flag = false;
-		while(scan.hasNext()){
+		boolean flag = false;
+		while (scan.hasNext()) {
 			String[] array = scan.nextLine().split("");
 
-			for(int i = 0;i<array.length;i++){
-				if(array[i].equals("[")||array[i].equals("{")||array[i].equals("[")) {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i].equals("[") || array[i].equals("{") || array[i].equals("[")) {
 					mystack.push(array[i]);
 
 				} else {
-					if(mystack.empty()) {
-						System.out.println("NO");
+					if (mystack.empty()) {
+						System.out.println("S");
 						flag = true;
 					}	else {
-								mystack.pop();
+						mystack.pop();
 					}
 				}
 			}
-			if(!flag){
+			if (!flag) {
 				System.out.println("YES");
-		    }
-		    else{
-		    	System.out.println("NO");
-		    }
-		    }
+			} else {
+				System.out.println("NO");
+			}
+		}
 
 
 
