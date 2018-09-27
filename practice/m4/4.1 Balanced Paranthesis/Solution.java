f6bc90023d1a4ba8156e6d5884f2192b11a817d7
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-27 17:13:40 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -7,53 +7,53 @@ import java.util.Stack;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructor for solution class.
-	 */
-	private Solution() {
+    /**
+     * Constructor for solution class.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { main method }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
+    }
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		int inputcount = Integer.parseInt(scan.nextLine());
+        Scanner scan = new Scanner(System.in);
+        int inputcount = Integer.parseInt(scan.nextLine());
 
-		for (int j = 0; j < inputcount; j++) {
-			boolean flag = false;
-			Stack<String> mystack = new Stack<String>();
-			String[] array = scan.nextLine().split("");
-			for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < inputcount; j++) {
+            boolean flag = false;
+            Stack<String> mystack = new Stack<String>();
+            String[] array = scan.nextLine().split("");
+            for (int i = 0; i < array.length; i++) {
                 if (array[i].equals("[") || array[i].equals("{")
                  || array[i].equals("(")) {
 
-					mystack.push(array[i]);
+                    mystack.push(array[i]);
 
-				} else {
-					if (mystack.isEmpty()) {
-						System.out.println("NO");
-						flag = true;
-						break;
-					}
-					mystack.pop();
+                } else {
+                    if (mystack.isEmpty()) {
+                        System.out.println("NO");
+                        flag = true;
+                        break;
+                    }
+                    mystack.pop();
 
-				}
-			}
+                }
+            }
 
-			if (!flag) {
-				if (mystack.isEmpty()) {
-					System.out.println("YES");
-				} else {
-					System.out.println("NO");
-				}
-			}
+            if (!flag) {
+                if (mystack.isEmpty()) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
 
 
-		}
-	}
+        }
+    }
 
 }
