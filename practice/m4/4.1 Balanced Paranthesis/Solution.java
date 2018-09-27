9aef777eb7f5adc3f8ef75c9337230f94111e779
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-27 16:49:12 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 import java.util.Stack;
 class Solution {
 	public static void main(String[] args) {
-		Stack<String> mystack = new Stack<String>();
+
 		Scanner scan = new Scanner(System.in);
 		int inputcount = Integer.parseInt(scan.nextLine());
 		boolean flag = false;
 		for(int j = 0; j<inputcount; j++) {
+			Stack<String> mystack = new Stack<String>();
 			String[] array = scan.nextLine().split("");
 			for (int i = 0; i < array.length; i++) {
 				if (array[i].equals("[") || array[i].equals("{") || array[i].equals("(")) {
