03dbc60a0fcc82667e300001db34041700226f29
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-28 17:52:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- practice/m5/5.1 Deque/Solution.java
+++ practice/m5/5.1 Deque/Solution.java
@@ -1,179 +1,178 @@
 import java.util.*;
 class Deque<E> {
-	 Node head;
-	 int size = 0;
+	Node head;
+	int size = 0;
 
-    class Node {
-    	E data;
-    	Node next;
+	class Node {
+		E data;
+		Node next;
 
-    	Node(E data1) {
-        	this.data = data1;
-        	next = null;
-       }
-    }
+		Node(E data1) {
+			this.data = data1;
+			next = null;
+		}
+	}
 
-    public int getsize(){
-    	return size;
-    }
+	public int getsize() {
+		return size;
+	}
 
-    public boolean isEmpty() {
-    	if(size == 0){
-    		return true;
-    	}
-    	return false;
-    }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
 
 
-    public void addFront(E data) {
-    	Node first = new Node(data);
-        if (size == 0) {
-        	head = first;
-        	size++;
-        	return;
-        }
-        first.next = head;
-        head = first;
-        size++;
-    }
+	public void addFront(E data) {
+		Node first = new Node(data);
+		if (size == 0) {
+			head = first;
+			size++;
+			return;
+		}
+		first.next = head;
+		head = first;
+		size++;
+	}
 
-    public void addLast(E data) {
-    	if(size == 0) {
-    		addFront(data);
-    		return;
-    	}
-    	Node last = new Node(data);
-    	Node n = head;
-    	int count = 0;
-    	while(count < size-1) {  /**because of head**/
-    		n = n.next;
-    		count++;
-    	}
-    	n.next = last;
-    	last.next = null;
-    	size++;
-    }
-    /**inserts at index ith position**/
-    // public void addAfter(int i, E data) {
-    // 	Node middle = new Node(data);
-    // 	Node n = head;
-    // 	int count = 0;
-    // 	while(count < i-1) {
-    // 		count++;
-    // 		n = n.next;
-    // 	}
-    // 	// System.out.println(n.data);
-    // 	middle.next = n.next;
-    // 	n.next = middle;
-    // 	size++;
-    // }
+	public void addLast(E data) {
+		if (size == 0) {
+			addFront(data);
+			return;
+		}
+		Node last = new Node(data);
+		Node n = head;
+		int count = 0;
+		while (count < size - 1) {
+			n = n.next;
+			count++;
+		}
+		n.next = last;
+		last.next = null;
+		size++;
+	}
+	/**inserts at index ith position**/
+	// public void addAfter(int i, E data) {
+	// 	Node middle = new Node(data);
+	// 	Node n = head;
+	// 	int count = 0;
+	// 	while(count < i-1) {
+	// 		count++;
+	// 		n = n.next;
+	// 	}
+	// 	// System.out.println(n.data);
+	// 	middle.next = n.next;
+	// 	n.next = middle;
+	// 	size++;
+	// }
 
-    public int removeFirst() {
-    	if (size == 0) {
-    		//System.out.println("Deck is empty");
-    		return 0;
-    	}
-        head = head.next;
-        size--;
-        return 1;
-    }
+	public int removeFirst() {
+		if (size == 0) {
+			//System.out.println("Deck is empty");
+			return 0;
+		}
+		head = head.next;
+		size--;
+		return 1;
+	}
 
-    public int removeLast() {
-    	if (size == 0) {
-    		//System.out.println("Deck is empty");
-    		return 0;
-    	}
-        Node n = head;
-        int count = 0;
-        while(count < size-2) {
-        	n = n.next;
-        	count++;
-        }
-        // System.out.println(n.data);
-        n.next = null;
-        size--;
-        return 1;
-    }
+	public int removeLast() {
+		if (size == 0) {
+			//System.out.println("Deck is empty");
+			return 0;
+		}
+		Node n = head;
+		int count = 0;
+		while (count < size - 2) {
+			n = n.next;
+			count++;
+		}
+		// System.out.println(n.data);
+		n.next = null;
+		size--;
+		return 1;
+	}
 
-    // public void removeAfter(int i) {
-    // 	Node n = head;
-    // 	int count = 0;
-    // 	while(count < i-1) {
-    // 		count++;
-    // 		n = n.next;
-    // 	}
-    // 	n.next = n.next.next;
-    // 	size--;
-    // }
-    //
+	// public void removeAfter(int i) {
+	// 	Node n = head;
+	// 	int count = 0;
+	// 	while(count < i-1) {
+	// 		count++;
+	// 		n = n.next;
+	// 	}
+	// 	n.next = n.next.next;
+	// 	size--;
+	// }
+	//
 
-    public String print() {
-    	if(size == 0){
-    		return "[]";
-    	}
-    	Node n = head;
-    	String str = "";
-    	str += "[";
-    	// while(n != null) {
-    	// 	str += n.data+",";
-    	// 	//System.out.print(n.data+", ");
-    	// 	n = n.next;
-    	// }
-    	for(int i = 0; i < size-1; i++){
-    		str += n.data+", ";
-    		n=n.next;
-    	}
-    	return str+n.data+"]";
+	public String print() {
+		if (size == 0) {
+			return "[]";
+		}
+		Node n = head;
+		String str = "";
+		str += "[";
+		// while(n != null) {
+		// 	str += n.data+",";
+		// 	//System.out.print(n.data+", ");
+		// 	n = n.next;
+		// }
+		// for(E i : o){
+		// }
+		for (int i = 0; i < size - 1; i++) {
+			str += n.data + ", ";
+			n = n.next;
+		}
+		return str + n.data + "]";
 
-    }
+	}
 
 
 }
 
 
-class Solution{
+class Solution {
 	public static void main(String[] args) {
 		Deque<String> obj = new Deque<String>();
 		Scanner scan = new Scanner(System.in);
 		int numberoflines = Integer.parseInt(scan.nextLine());
-		while(scan.hasNext()){
+		while (scan.hasNext()) {
 			String[] firsttoken = scan.nextLine().split(" ");
-			switch(firsttoken[0]){
-				case "pushLeft":
-				      obj.addFront(firsttoken[1]);
-				      System.out.println(obj.print());
+			switch (firsttoken[0]) {
+			case "pushLeft":
+				obj.addFront(firsttoken[1]);
+				System.out.println(obj.print());
 
 				break;
-				case "pushRight":
-				      obj.addLast(firsttoken[1]);
-				      System.out.println(obj.print());
+			case "pushRight":
+				obj.addLast(firsttoken[1]);
+				System.out.println(obj.print());
 				break;
-				case "popLeft" :
-				      int i = obj.removeFirst();
-				      if (i == 1){
-				      System.out.println(obj.print());
-				      } else {
-				      System.out.println("Deck is empty");
-				  }
+			case "popLeft" :
+				int i = obj.removeFirst();
+				if (i == 1) {
+					System.out.println(obj.print());
+				} else {
+					System.out.println("Deck is empty");
+				}
 				break;
-				case "popRight" :
-				      int j = obj.removeLast();
-				      if(j == 1){
-				      System.out.println(obj.print());
-				      } else {
-				      System.out.println("Deck is empty");
-				     }
+			case "popRight" :
+				int j = obj.removeLast();
+				if (j == 1) {
+					System.out.println(obj.print());
+				} else {
+					System.out.println("Deck is empty");
+				}
 				break;
-				case "size" :
-				      System.out.println(obj.getsize());
+			case "size" :
+				System.out.println(obj.getsize());
 
 				break;
-				case "isEmpty" :
-				      System.out.println(obj.isEmpty());
+			case "isEmpty" :
+				System.out.println(obj.isEmpty());
 
 				break;
-				default:
+			default:
 				break;
 			}
 		}
