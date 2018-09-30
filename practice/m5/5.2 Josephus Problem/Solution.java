bbe33a5e5d33c4e758c3f6a27bdaf398d3cd604a
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-30 21:20:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- practice/m5/5.2 Josephus Problem/Solution.java
+++ practice/m5/5.2 Josephus Problem/Solution.java
@@ -1,88 +1,125 @@
+/**
+ * Author:Gnanika.
+ */
 import java.util.Scanner;
+/**
+ * queue class.
+ */
 class Queue {
+	/**
+	 * head pointer.
+	 */
 	private Node head;
+	/**
+	 * tail pointer.
+	 */
 	private Node tail;
-    //public int size;
+	//public int size;
 
-    private class Node {
-        private int value;
-        private Node next;
-    }
+    /**
+     * Class for creating node.
+     */
+	private class Node {
+		private int value;
+		private Node next;
+	}
 
-    Queue() {
-        head = null;
-        tail = null;
-    }
+	/**
+	 * Constructor for queue.
+	 */
+	Queue() {
+		head = null;
+		tail = null;
+	}
 
-    public void enqueue (final int value){
-    	if (tail == null) {
-           tail = new Node();
-           tail.value = value;
-           tail.next = null;
+	/**
+	 * method for pushing into queue from right.
+	 *
+	 * @param      value  The value
+	 */
 
-           head = tail;
+	public void enqueue (final int value) {
+		if (tail == null) {
+			tail = new Node();
+			tail.value = value;
+			tail.next = null;
 
-        } else {
-           Node oldTail = tail;
-    	   tail = new Node();
-    	   tail.value = value;
-    	   tail.next = null;
-           oldTail.next = tail;
+			head = tail;
 
-        }
-
-    }
-
-    public int dequeue(){
-        if (head != null) {
-            int value = head.value;
-            head = head.next;
-            //size--;
-            return value;
-        }
-        return tail.value;
-    }
-
-    public boolean isEmpty() {
-        return head == null;
-    }
-    // public int size() {
-    //     return size;
-    // }
-}
-
-
-
-
-
-
-
-
-class Solution{
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int number = Integer.parseInt(scan.nextLine());
-		for(int i = 0;i<number;i++){
-			String[] token= scan.nextLine().split(" ");
-			int totalpeople = Integer.parseInt(token[0]);
-            int shift = Integer.parseInt(token[1]);
-            Queue q = new Queue();
-            String str = "";
-            for (int j = 0; j < totalpeople; j++) {
-                q.enqueue(j);
-            }
-
-            while (!q.isEmpty()) {
-                for (int k = 0; k < shift - 1; k++) {
-                    q.enqueue(q.dequeue());
-                }
-                str += q.dequeue() + " ";
-            }
-            System.out.println(str.substring(0, str.length() - 1));
-            //System.out.println();
-        }
-
+		} else {
+			Node oldTail = tail;
+			tail = new Node();
+			tail.value = value;
+			tail.next = null;
+			oldTail.next = tail;
 
 		}
 
 	}
+
+    /**
+     * method for popping from the queue from left most place.
+     *
+     * @return     { description_of_the_return_value }
+     */
+	public int dequeue() {
+		if (head != null) {
+			int value = head.value;
+			head = head.next;
+			//size--;
+			return value;
+		}
+		return tail.value;
+	}
+
+    public boolean isEmpty() {
+		return head == null;
+	}
+	// public int size() {
+	//     return size;
+	// }
+}
+
+
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructor for solution.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < number; i++) {
+			String[] token = scan.nextLine().split(" ");
+			int totalpeople = Integer.parseInt(token[0]);
+			int shift = Integer.parseInt(token[1]);
+			Queue q = new Queue();
+			String str = "";
+			for (int j = 0; j < totalpeople; j++) {
+				q.enqueue(j);
+			}
+
+			while (!q.isEmpty()) {
+				for (int k = 0; k < shift - 1; k++) {
+					q.enqueue(q.dequeue());
+				}
+				str += q.dequeue() + " ";
+			}
+			System.out.println(str.substring(0, str.length() - 1));
+			//System.out.println();
+		}
+
+
+	}
+
+}
