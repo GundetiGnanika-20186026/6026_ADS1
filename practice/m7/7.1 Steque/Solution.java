659f65f07a792258cc6533a8fbc46b6bce3aa988
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-01 12:08:19 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- practice/m7/7.1 Steque/Solution.java
+++ practice/m7/7.1 Steque/Solution.java
@@ -95,7 +95,7 @@ class Solution {
 
 			while (scan.hasNext()) {
 				String[] array = scan.nextLine().split(" ");
-				if (array[0].equals(" ")){
+				if (array[0].equals("")){
 					break;
 				}
 
