14ddba07c7ff84de1a05a5c255b4a09f850c880e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-01 14:31:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- practice/m4/4.2 Evaluate Postfix/Solution.java
+++ practice/m4/4.2 Evaluate Postfix/Solution.java
@@ -1,71 +1,117 @@
+/**
+ * Author:Gnanika
+ */
 import java.util.Scanner;
 //import java.util.Arrays;
-class Linkedlist{
-	 Node head = null;
-	 int size = 0;
-	 class Node{
-		 int data;
-		 Node next;
-		Node(int data1){
+
+/**
+ * Class for linkedlist.
+ */
+class Linkedlist {
+	/**
+	 * { head pointer }.
+	 */
+	private Node head = null;
+	/**
+	 * { variable to keep track of size }.
+	 */
+	private int size = 0;
+	/**
+	 * Class for node.
+	 */
+	private class Node {
+		/**
+		 * { data in a node}.
+		 */
+		private int data;
+		/**
+		 * { address that points next node }.
+		 */
+		private Node next;
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      data1  The data 1
+		 */
+		Node(final int data1) {
 			data = data1;
 			next = null;
 		}
 	}
 
-		public void push(int data1){
-			Node newnode = new Node(data1);
-			if(size == 0){
-				head = newnode;
-				size++;
-				return;
+	/**
+	 * { push the node in to linked list at start}.
+	 *
+	 * @param      data1  The data 1
+	 */
+	public void push(final int data1) {
+		Node newnode = new Node(data1);
+		if (size == 0) {
+			head = newnode;
+			size++;
+			return;
 
-			}
-            newnode.next = head;
-            head = newnode;
-            size++;
+		}
+		newnode.next = head;
+		head = newnode;
+		size++;
 
-        }
-
-        public int pop(){
-            int value = head.data;
-            head = head.next;
-            size--;
-            return value;
-
-
-        }
-
-        public void print(){
-        	System.out.println(head.data);
-        }
 	}
 
-class Solution{
-	public static void main(String[] args) {
+	/**
+	 * { pop from the beginning }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int pop() {
+		int value = head.data;
+		head = head.next;
+		size--;
+		return value;
+
+
+	}
+
+	public void print() {
+		System.out.println(head.data);
+	}
+}
+
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { main method }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Linkedlist obj = new Linkedlist();
 		Scanner scan = new Scanner(System.in);
 		String[] array = scan.nextLine().split(" ");
 		//System.out.println(Arrays.toString(array));
 		for (int i = 0; i < array.length; i++) {
-            // if(!array[i].equals("+")||!array[i].equals("*")){
+			// if(!array[i].equals("+")||!array[i].equals("*")){
 
 
-            if (array[i].equals("+")) {
-            	int value1 = obj.pop();
-            	int value2 = obj.pop();
-            	int result = value1+value2;
-            	obj.push(result);
+			if (array[i].equals("+")) {
+				int value1 = obj.pop();
+				int value2 = obj.pop();
+				int result = value1 + value2;
+				obj.push(result);
 
-            }
-            else if (array[i].equals("*")) {
-            	int value1 = obj.pop();
-            	int value2 = obj.pop();
-            	int result = value1 * value2;
-            	obj.push(result);
-            }
-            else{
-            obj.push(Integer.parseInt(array[i]));
-        }
+			} else if (array[i].equals("*")) {
+				int value1 = obj.pop();
+				int value2 = obj.pop();
+				int result = value1 * value2;
+				obj.push(result);
+			} else {
+				obj.push(Integer.parseInt(array[i]));
+			}
 
 
 		}
