e0c0c9e341bce06e6b63600a36df18bebb956a1f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-02 18:06:14 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- practice/m8/8.1 Team Ranking With Insertion/Solution.java
+++ practice/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,12 +1,30 @@
 //import java.util.Comparator;
+
+/**
+ * Author:Gnanika.
+ */
 import java.util.Scanner;
+/**
+ * Class for team which contains all the data such as
+ *  teamname,wins,losses and draws.
+ */
 class Team {
 	String teamname;
 	int wins;
 	int losses;
 	int draws;
+	/**
+	 * empty Constructor.
+	 */
 	Team() { }
-
+    /**
+     * parameterised constructor.
+     *
+     * @param      name     The name
+     * @param      wins1    The wins 1
+     * @param      losses1  The losses 1
+     * @param      draws1   The draws 1
+     */
 	Team(final String name, final int wins1, final int losses1, final int draws1) {
 		this.teamname = name;
 		this.wins = wins1;
@@ -14,9 +32,11 @@ class Team {
 		this.draws = draws1;
 	}
 
-	// @Override
+	// //@Override
 
-	// public int compareTo(){
+	// public int compareTo(Team team1) {
+	// 	return this.wins - team1.wins;
+
 
 	// }
 }
@@ -28,18 +48,36 @@ class Team {
 
 ////////////////////////////////////////////////////////////////////////
 
+/**
+ * Class for teamarray this array contains all team objects.
+ */
 class Teamarray {
 	Team[] teamarray;
 	int size;
+	/**
+	 * Constructor for team array.
+	 */
 	Teamarray() {
 		teamarray = new Team[50];
 		size = 0;
 
 	}
+
+	/**
+	 * { this method adds the team objects into team array }.
+	 * its complexity is O(1).
+	 *
+	 * @param      obj1  The object 1
+	 */
 	public void add(Team obj1) {
 		teamarray[size++] = obj1;
 
 	}
+
+	/**
+	 * { this method is used to send the array to the insertion sort class }.
+	 * its complexity is O(1)
+	 */
 
 	public void sendarray(){
 		Insertionsort sortobj = new Insertionsort();
@@ -47,7 +85,10 @@ class Teamarray {
 	}
 
 
-
+    /**
+     * { This method is used to print the teamarray which is sorted }
+     * its complexity is O(n) as it itterates through the entire array.
+     */
 	public void print() {
 
 		String str = "";
@@ -61,11 +102,25 @@ class Teamarray {
 }
 
 /////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Class for insertionsort which contains sort method for sorting.
+ */
 class Insertionsort {
+	/**
+	 * Constructor for insertion sort.
+	 */
 	Insertionsort() {
 
 	}
-
+    /**
+     * { will exchange the objects in the team array}.
+     * its complexity is O(1).
+     *
+     * @param      teamarray  The teamarray
+     * @param      index1     The index 1
+     * @param      index2     The index 2
+     */
 	public void exchange(final Team[] teamarray, final int index1, final int index2) {
 		Team extra = new Team();
 		extra = teamarray[index1];
@@ -73,7 +128,16 @@ class Insertionsort {
 		teamarray[index2] = extra;
 	}
 
+	/**
+	 * { this method will perform the insertion sort  }.
+	 * its complexity is O(n^2)
+	 *
+	 * @param      a      { this is the teamarray }
+	 * @param      size1  The size 1
+	 */
+
 	public void sort(Team[] a,int size1) {
+
 		for (int i = 0; i < size1; i++) {
 			//int max = i;
 			for (int j = i; j > 0; j--) {
@@ -100,7 +164,21 @@ class Insertionsort {
 
 //////////////////////////////////////////////////////////////////////
 
-class Solution {
+/**
+ * Class for solution which contains main method.
+ */
+final class Solution {
+	/**
+	 * Constructor for solution.
+	 */
+	private Solution(){
+
+	}
+    /**
+     * { this is the main method to read the user input}.
+     *
+     * @param      args  The arguments
+     */
 
 	public static void main(final String[] args) {
 
