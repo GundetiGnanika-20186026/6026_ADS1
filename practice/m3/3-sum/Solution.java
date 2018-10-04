6c5d5e68957416ce845e1060a0d1c62a7c6d8b49
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 10:07:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- practice/m3/3-sum/Solution.java
+++ practice/m3/3-sum/Solution.java
@@ -68,13 +68,51 @@ final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int arraysize = scan.nextInt();
-        int count = 0;
-//Threesum object1 = new Threesum(arraysize);
+
+
+
         int[] array = new int[arraysize];
         for (int i = 0; i < arraysize; i++) {
             //object1.add(scan.nextInt());
             array[i] = scan.nextInt();
         }
+
+        Threesum object1 = new Threesum();
+         object1.threesum(arraysize,array);
+//         Arrays.sort(array);
+//         for (int i = 0; i < arraysize; i++) {
+//             for (int j = i + 1; j < arraysize; j++) {
+//     //sending the values to the binary search and
+//     // searching wether it is present.
+// int k = Arrays.binarySearch(array, -(array[i] + array[j]));
+//                 if (k > j) {
+//                     count = count + 1;
+//                 }
+//             }
+//         }
+//         System.out.println(count);
+
+
+        //System.out.println(Arrays.toString(object1.getarray()));
+        //object1.sumofthree();
+
+
+    }
+}
+
+
+class Threesum{
+    Threesum(){
+
+    }
+    public void threesum(final int arraysize,final int[] array){
+
+        //int[] array = new int[arraysize];
+        // for (int i = 0; i < arraysize; i++) {
+        //     //object1.add(scan.nextInt());
+        //     array[i] = scan.nextInt();
+        // }
+         int count = 0;
         Arrays.sort(array);
         for (int i = 0; i < arraysize; i++) {
             for (int j = i + 1; j < arraysize; j++) {
@@ -87,11 +125,6 @@ int k = Arrays.binarySearch(array, -(array[i] + array[j]));
             }
         }
         System.out.println(count);
-
-
-        //System.out.println(Arrays.toString(object1.getarray()));
-        //object1.sumofthree();
-
 
     }
 }
