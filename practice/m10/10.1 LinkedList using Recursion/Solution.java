064f7538b62957dcc8916b69096c7461792e116e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 22:18:06 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m10/10.1 LinkedList using Recursion/Solution.java
+++ practice/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
+/**
+ * Class for linkedlist.
+ */
 class Linkedlist {
 	Node head = null;
 	Node test;
 	Node prev;
 	Node temp;
 	int size = 0;
+	/**
+	 * Class for node.
+	 */
 	class Node {
 		int data;
 		Node next;
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      data1  The data 1
+		 */
 		Node(int data1) {
 			this.data = data1;
 			this.next = null;
 		}
 	}
-
-	public void insertAt(int index, int data) throws Exception {
+    /**
+     * { this method will insert the element at specific index }.
+     * its complexity is O(n) as we iterate through the list
+     *
+     * @param      index      The index
+     * @param      data       The data
+     *
+     * @throws     Exception  { exception_description }
+     */
+	public void insertAt(int index,
+	 int data) throws Exception {
 		// Node newnode = new Node(data);
 		// if (size == 0) {
 		// 	head = newnode;
@@ -31,7 +51,8 @@ class Linkedlist {
   //       Node n = head;
 
 		if (index > size || index < 0) {
-			throw new Exception("Can't insert at this position.");
+			throw new Exception(
+				"Can't insert at this position.");
 			// return;
 		}
 
@@ -56,10 +77,16 @@ class Linkedlist {
 		index--;
 		insertAt(index, data);
 }
-
+    /**
+     * { this method  will reverse the linked list }.
+     * the complexity of this method is O(n).
+     *
+     * @throws     Exception  { throws exception if present }
+     */
 	public void reverse() throws Exception {
 		if (size == 0) {
-			throw new Exception("No elements to reverse.");
+			throw new Exception(
+				"No elements to reverse.");
 		}
 		if (test.next == null) {
 			head = test;
@@ -75,7 +102,10 @@ class Linkedlist {
 		reverse();
 
 	}
-
+    /**
+     * Method to print in required way.
+     * the complexity of this method is O(n).
+     */
 	public void print(){
 
 		String str = "";
@@ -91,10 +121,21 @@ class Linkedlist {
 
 }
 
-class Solution {
-	Solution() {
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
 
 	}
+	/**
+	 * { main method }.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Linkedlist object = new Linkedlist();
 		Scanner scan = new Scanner(System.in);
@@ -103,7 +144,8 @@ class Solution {
 			switch (token[0]) {
 			case "insertAt" :
 			    try {
-				object.insertAt(Integer.parseInt(token[1]), Integer.parseInt(token[2]));
+				object.insertAt(Integer.parseInt(token[1]),
+				 Integer.parseInt(token[2]));
 				object.print();
 			    }
 			    catch (Exception e) {
