53a42e0406fd4231e8aa694fe7b8cd4257c6569d
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 09:58:00 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- practice/m3/Sorted_Array/Solution.java
+++ practice/m3/Sorted_Array/Solution.java
@@ -20,6 +20,48 @@ final class Solution {
         String[] str2 = scan.nextLine().split(",");
         int[] arr1 = new int[arr1size];
         int[] arr2 = new int[arr2size];
+        Sortedarray obj = new Sortedarray();
+        obj.sort(str1,str2,arr1,arr2,arr1size,arr2size);
+
+//         for (int i = 0; i < arr1size; i++) {
+//             arr1[i] = Integer.parseInt(str1[i]);
+//         }
+
+//         for (int i = 0; i < arr2size; i++) {
+//             arr2[i] = Integer.parseInt(str2[i]);
+//         }
+
+//         int[] arr3 = new int[arr1size + arr2size];
+//         int i = 0; int j = 0; int k = 0;
+//         while (i < arr1size && j < arr2size) {
+//             if (arr1[i] < arr2[j]) {
+//                 arr3[k++] = arr1[i++];
+//             }
+//             arr3[k++] = arr2[j++];
+
+//         }
+//         while (i < arr1size) {
+//             arr3[k++] = arr1[j++];
+//         }
+//         while (j < arr2size) {
+//             arr3[k++] = arr2[j++];
+//         }
+//         int a;
+// for (a = 0; a < (arr1size + arr2size) - 1; a++) {
+//             System.out.print(arr3[a] + ",");
+//         }
+//         System.out.print(arr3[a]);
+
+    }
+}
+
+class Sortedarray{
+
+    Sortedarray(){
+
+    }
+
+    public void sort(String[] str1,String[] str2,int[] arr1, int[] arr2,int arr1size,int arr2size){
         for (int i = 0; i < arr1size; i++) {
             arr1[i] = Integer.parseInt(str1[i]);
         }
@@ -48,6 +90,6 @@ for (a = 0; a < (arr1size + arr2size) - 1; a++) {
             System.out.print(arr3[a] + ",");
         }
         System.out.print(arr3[a]);
-
     }
+
 }
