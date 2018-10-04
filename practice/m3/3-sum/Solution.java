832ccb4730c4fb9c923f3eb93317039153d205a7
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 10:09:57 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- practice/m3/3-sum/Solution.java
+++ practice/m3/3-sum/Solution.java
@@ -3,53 +3,6 @@ import java.util.Arrays;
 /**
  *Author:Gnanika
  */
-
-// class Threesum {
-//  private int[] array;
-//  private int size;
-
-//  Threesum(int arraysize) {
-//      array = new int[arraysize];
-//      size = 0;
-//  }
-
-//  public int[] getarray() {
-//      return array;
-//  }
-
-//  public void add(int value) {
-//      array[size++] = value;
-
-//  }
-
-//  public void sumofthree() {
-//      int count = 0;
-//      Arrays.sort(array);
-//      int sum = 0;
-
-//         //System.out.println(size);
-//      for (int i = 0; i < size ; i++) {
-
-//          for (int j = i + 1; j < size; j++) {
-
-//              for (int k = j + 1; k < size; k++) {
-//              sum = array[i] + array[j] + array[k];
-//                  if (sum == 0) {
-//                      count = count + 1;
-//                  }
-//              }
-//          }
-//      }
-//      System.out.println(count);
-//  }
-
-
-
-
-
-
-// }
-
 /**
  * Class for solution.
  */
@@ -79,22 +32,7 @@ final class Solution {
 
         Threesum object1 = new Threesum();
         object1.threesum(arraysize, array);
-//         Arrays.sort(array);
-//         for (int i = 0; i < arraysize; i++) {
-//             for (int j = i + 1; j < arraysize; j++) {
-//     //sending the values to the binary search and
-//     // searching wether it is present.
-// int k = Arrays.binarySearch(array, -(array[i] + array[j]));
-//                 if (k > j) {
-//                     count = count + 1;
-//                 }
-//             }
-//         }
-//         System.out.println(count);
 
-
-        //System.out.println(Arrays.toString(object1.getarray()));
-        //object1.sumofthree();
 
 
     }
