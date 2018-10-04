267449c8364dff7d4ceb6c80a586c75ad0c587ae
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 22:33:14 +0530

    "ads1_m10_a1: 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m10/10.1 LinkedList using Recursion/Solution.java
+++ practice/m10/10.1 LinkedList using Recursion/Solution.java
@@ -3,48 +3,48 @@ import java.util.Scanner;
  * Class for linkedlist.
  */
 class Linkedlist {
-	/**
-	 * head node.
-	 */
-	private Node head = null;
-	/**
-	 * extra node for iteration.
-	 */
-	private Node extra;
-	/**
-	 * prev node pointer
-	 */
-	private Node prev;
-	/**
-	 * temporary node.
-	 */
-	private Node temp;
-	/**
-	 * variable to maintain size.
-	 */
-	private int size = 0;
-	/**
-	 * Class for node.
-	 */
-	class Node {
-		/**
-		 * data of the node.
-		 */
-		private int data;
-		/**
-		 * address of next node is Stored.
-		 */
-		private Node next;
-		/**
-		 * Constructs the object.
-		 *
-		 * @param      data1  The data 1
-		 */
-		Node(final int data1) {
-			this.data = data1;
-			this.next = null;
-		}
-	}
+    /**
+     * head node.
+     */
+    private Node head = null;
+    /**
+     * extra node for iteration.
+     */
+    private Node extra;
+    /**
+     * prev node pointer.
+     */
+    private Node prev;
+    /**
+     * temporary node.
+     */
+    private Node temp;
+    /**
+     * variable to maintain size.
+     */
+    private int size = 0;
+    /**
+     * Class for node.
+     */
+    class Node {
+        /**
+         * data of the node.
+         */
+        private int data;
+        /**
+         * address of next node is Stored.
+         */
+        private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      data1  The data 1
+         */
+        Node(final int data1) {
+            this.data = data1;
+            this.next = null;
+        }
+    }
     /**
      * { this method will insert the element at specific index }.
      * its complexity is O(n) as we iterate through the list
@@ -54,49 +54,49 @@ class Linkedlist {
      *
      * @throws     Exception  { exception_description }
      */
-	public void insertAt(int index,
-	 final int data) throws Exception {
-		// Node newnode = new Node(data);
-		// if (size == 0) {
-		// 	head = newnode;
-		// 	size++;
-		// 	return;
-		// }
-		// if (index == 0) {
-		// 	newnode.next = head;
-		// 	head = newnode;
-		// 	size++;
-		// 	return;
-		// }
+    public void insertAt(int index,
+     final int data) throws Exception {
+        // Node newnode = new Node(data);
+        // if (size == 0) {
+        //  head = newnode;
+        //  size++;
+        //  return;
+        // }
+        // if (index == 0) {
+        //  newnode.next = head;
+        //  head = newnode;
+        //  size++;
+        //  return;
+        // }
 
   //       Node n = head;
+        final int in1 = index;
+        if (in1 > size || in1 < 0) {
+            throw new Exception(
+                "Can't insert at this position.");
+            // return;
+        }
 
-		if (index > size || index < 0) {
-			throw new Exception(
-				"Can't insert at this position.");
-			// return;
-		}
+        if (in1 == 0) {
+            Node newnode = new Node(data);
+            newnode.next = extra;
+            head = newnode;
+            extra =  head;
+            size++;
+            return;
+        }
+        if (in1 == 1) {
+            Node newnode = new Node(data);
+            newnode.next = extra.next;
+            extra.next = newnode;
+            extra = head;
+            size++;
+            return;
+        }
 
-		if (index == 0) {
-			Node newnode = new Node(data);
-			newnode.next = extra;
-			head = newnode;
-			extra =  head;
-			size++;
-			return;
-		}
-		if (index == 1) {
-			Node newnode = new Node(data);
-			newnode.next = extra.next;
-			extra.next = newnode;
-			extra = head;
-			size++;
-			return;
-		}
-
-		extra = extra.next;
-		index--;
-		insertAt(index, data);
+        extra = extra.next;
+        index--;
+        insertAt(in1, data);
 }
     /**
      * { this method  will reverse the linked list }.
@@ -104,41 +104,41 @@ class Linkedlist {
      *
      * @throws     Exception  { throws exception if present }
      */
-	public void reverse() throws Exception {
-		if (size == 0) {
-			throw new Exception(
-				"No elements to reverse.");
-		}
-		if (extra.next == null) {
-			head = extra;
-			head.next = prev;
-			extra = head;
-			prev = null;
-			return;
-		}
-		temp = extra;
-		extra = extra.next;
-		temp.next = prev;
-		prev = temp;
-		reverse();
+    public void reverse() throws Exception {
+        if (size == 0) {
+            throw new Exception(
+                "No elements to reverse.");
+        }
+        if (extra.next == null) {
+            head = extra;
+            head.next = prev;
+            extra = head;
+            prev = null;
+            return;
+        }
+        temp = extra;
+        extra = extra.next;
+        temp.next = prev;
+        prev = temp;
+        reverse();
 
-	}
+    }
     /**
      * Method to print in required way.
      * the complexity of this method is O(n).
      */
-	public void print(){
+    public void print() {
 
-		String str = "";
-		Node newextra = head;
-		while (newextra.next != null) {
-			str = str + newextra.data + ", ";
-			newextra = newextra.next;
-		}
-		str = str + newextra.data;
-		System.out.println(str);
+        String str = "";
+        Node newextra = head;
+        while (newextra.next != null) {
+            str = str + newextra.data + ", ";
+            newextra = newextra.next;
+        }
+        str = str + newextra.data;
+        System.out.println(str);
 
-	}
+    }
 
 }
 
@@ -146,45 +146,44 @@ class Linkedlist {
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { main method }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Linkedlist object = new Linkedlist();
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String[] token = scan.nextLine().split(" ");
-			switch (token[0]) {
-			case "insertAt" :
-			    try {
-				object.insertAt(Integer.parseInt(token[1]),
-				 Integer.parseInt(token[2]));
-				object.print();
-			    }
-			    catch (Exception e) {
-			    	System.out.println(e.getMessage());
-			    }
-				break;
-			case "reverse" :
-			    try {
-				object.reverse();
-				object.print();
-			    } catch (Exception e) {
-			    	System.out.println(e.getMessage());
-			    }
-				break;
-			default:
-			}
+    }
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Linkedlist object = new Linkedlist();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] token = scan.nextLine().split(" ");
+            switch (token[0]) {
+            case "insertAt" :
+                try {
+                object.insertAt(Integer.parseInt(token[1]),
+                 Integer.parseInt(token[2]));
+                object.print();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "reverse" :
+                try {
+                object.reverse();
+                object.print();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+            }
 
-		}
+        }
 
-	}
+    }
 }
