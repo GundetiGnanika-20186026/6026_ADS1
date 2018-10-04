910c11e88f07be905ce561f550714d87de74921d
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 09:47:19 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -24,9 +24,54 @@ final class Solution {
         int inputcount = Integer.parseInt(scan.nextLine());
 
         for (int j = 0; j < inputcount; j++) {
-            boolean flag = false;
-            Stack<String> mystack = new Stack<String>();
+            //boolean flag = false;
+            //Stack<String> mystack = new Stack<String>();
             String[] array = scan.nextLine().split("");
+            Balancedparanthesis obj = new Balancedparanthesis();
+            obj.balanced(array);
+        //     for (int i = 0; i < array.length; i++) {
+        //         if (array[i].equals("[") || array[i].equals("{")
+        //          || array[i].equals("(")) {
+
+        //             mystack.push(array[i]);
+
+        //         } else {
+        //             if (mystack.isEmpty()) {
+        //                 System.out.println("NO");
+        //                 flag = true;
+        //                 break;
+        //             }
+        //             mystack.pop();
+
+        //         }
+        //     }
+
+        //     if (!flag) {
+        //         if (mystack.isEmpty()) {
+        //             System.out.println("YES");
+        //         } else {
+        //             System.out.println("NO");
+        //         }
+        //     }
+
+
+         }
+    }
+
+}
+
+
+class Balancedparanthesis{
+    Balancedparanthesis(){
+
+    }
+    public void balanced(String[] array) {
+     // for (int j = 0; j < inputcount; j++) {
+     //        boolean flag = false;
+     //        Stack<String> mystack = new Stack<String>();
+     //        String[] array = scan.nextLine().split("");
+      Stack<String> mystack = new Stack<String>();
+             boolean flag = false;
             for (int i = 0; i < array.length; i++) {
                 if (array[i].equals("[") || array[i].equals("{")
                  || array[i].equals("(")) {
@@ -54,6 +99,6 @@ final class Solution {
 
 
         }
+
     }
 
-}
