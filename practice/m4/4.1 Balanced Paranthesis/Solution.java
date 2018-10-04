caba9708160f68bc8f44d6293cfefa39f3d1e72f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-04 09:49:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -29,76 +29,87 @@ final class Solution {
             String[] array = scan.nextLine().split("");
             Balancedparanthesis obj = new Balancedparanthesis();
             obj.balanced(array);
-        //     for (int i = 0; i < array.length; i++) {
-        //         if (array[i].equals("[") || array[i].equals("{")
-        //          || array[i].equals("(")) {
+            //     for (int i = 0; i < array.length; i++) {
+            //         if (array[i].equals("[") || array[i].equals("{")
+            //          || array[i].equals("(")) {
 
-        //             mystack.push(array[i]);
+            //             mystack.push(array[i]);
 
-        //         } else {
-        //             if (mystack.isEmpty()) {
-        //                 System.out.println("NO");
-        //                 flag = true;
-        //                 break;
-        //             }
-        //             mystack.pop();
+            //         } else {
+            //             if (mystack.isEmpty()) {
+            //                 System.out.println("NO");
+            //                 flag = true;
+            //                 break;
+            //             }
+            //             mystack.pop();
 
-        //         }
-        //     }
+            //         }
+            //     }
 
-        //     if (!flag) {
-        //         if (mystack.isEmpty()) {
-        //             System.out.println("YES");
-        //         } else {
-        //             System.out.println("NO");
-        //         }
-        //     }
+            //     if (!flag) {
+            //         if (mystack.isEmpty()) {
+            //             System.out.println("YES");
+            //         } else {
+            //             System.out.println("NO");
+            //         }
+            //     }
 
 
-         }
+        }
     }
 
 }
 
-
-class Balancedparanthesis{
-    Balancedparanthesis(){
+/**
+ * Class for balancedparanthesis.
+ */
+class Balancedparanthesis {
+    /**
+     * Constructs the object.
+     */
+    Balancedparanthesis() {
 
     }
-    public void balanced(String[] array) {
-     // for (int j = 0; j < inputcount; j++) {
-     //        boolean flag = false;
-     //        Stack<String> mystack = new Stack<String>();
-     //        String[] array = scan.nextLine().split("");
-      Stack<String> mystack = new Stack<String>();
-             boolean flag = false;
-            for (int i = 0; i < array.length; i++) {
-                if (array[i].equals("[") || array[i].equals("{")
-                 || array[i].equals("(")) {
+    /**
+     * { this function will check wether the given
+     *  paranthesis sequence is balanced }
+     *
+     * @param      array  The string array
+     */
+    public void balanced(final String[] array) {
+        // for (int j = 0; j < inputcount; j++) {
+        //        boolean flag = false;
+        //        Stack<String> mystack = new Stack<String>();
+        //        String[] array = scan.nextLine().split("");
+        Stack<String> mystack = new Stack<String>();
+        boolean flag = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals("[") || array[i].equals("{")
+                    || array[i].equals("(")) {
 
-                    mystack.push(array[i]);
+                mystack.push(array[i]);
 
-                } else {
-                    if (mystack.isEmpty()) {
-                        System.out.println("NO");
-                        flag = true;
-                        break;
-                    }
-                    mystack.pop();
-
-                }
-            }
-
-            if (!flag) {
+            } else {
                 if (mystack.isEmpty()) {
-                    System.out.println("YES");
-                } else {
                     System.out.println("NO");
+                    flag = true;
+                    break;
                 }
+                mystack.pop();
+
             }
-
-
         }
 
+        if (!flag) {
+            if (mystack.isEmpty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+
     }
+
+}
 
