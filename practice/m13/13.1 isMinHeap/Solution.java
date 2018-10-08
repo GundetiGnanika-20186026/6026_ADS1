7694433f9d6a2a6385d85d8918eaf24251016686
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-08 17:16:50 +0530

    "ads1_m13_a1: 1/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ practice/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+class MinPQ<Key> {
+    private  Key[] pq;
+    private  int n;
+    Comparator<Key> comparator;
+    MinPQ(Key[] array, int size) {
+        pq = array;
+        n = size;
+   }
+
+
+
+    boolean isMinHeap() {
+        return isMinHeap(1);
+    }
+
+    // is subtree of pq[1..n] rooted at k a min heap?
+    private boolean isMinHeap(int k) {
+        if (k > n) return true;
+        int left = 2*k;
+        int right = 2*k + 1;
+        if (left  <= n && greater(k, left))  return false;
+        if (right <= n && greater(k, right)) return false;
+        return isMinHeap(left) && isMinHeap(right);
+    }
+
+     private boolean greater(int i, int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+        }
+        else {
+            return comparator.compare(pq[i], pq[j]) > 0;
+        }
+    }
+
+
+
+
+
+
+}
+
+
+class Solution {
+    Solution() {
+
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String type = scan.nextLine();
+        switch(type){
+            case "String":
+
+                int testcase = Integer.parseInt(scan.nextLine());
+                for(int i = 0; i< testcase; i++){
+                    String[] input = scan.nextLine().split(",");
+                    MinPQ<String> obj = new MinPQ<String>(input,input.length);
+                   System.out.println(obj.isMinHeap());
+                }
+
+            break;
+            // case "Integer" :
+
+            //     int testcase1 = Integer.parseInt(scan.nextLine());
+            //     for(int i = 0; i< testcase1; i++){
+            //         Integer[] input = scan.nextLine().split(",");
+            //         MinPQ<Integer> obj = new MinPQ<Integer>(input,input.length);
+            //        obj.isMinHeap();
+
+            //     }
+
+            // break;
+        }
+    }
+
+
+
+
+
+}
