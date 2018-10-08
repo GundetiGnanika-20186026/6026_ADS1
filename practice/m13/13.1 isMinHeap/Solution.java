5551f7d61ed60ad1590392cac8b1c000ce5bf67e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-08 19:34:04 +0530

    "ads1_m13_a1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m13/13.1 isMinHeap/Solution.java
+++ practice/m13/13.1 isMinHeap/Solution.java
@@ -9,7 +9,7 @@ class MinPQ<Key extends Comparable<Key>> {
     // Comparator<Key> comparator;
     MinPQ() {
 
-   }
+    }
 
 
 
@@ -36,16 +36,15 @@ class MinPQ<Key extends Comparable<Key>> {
     //     }
     // }
     public boolean minHeapchecking(Key[] array) {
-        int n = array.length-1;
-        if (array.length == 0) {
-            return false;
-        }
-        while(n>1) {
+        int n = array.length - 1;
+        // if (array.length == 0) {
+        //     return false;
+        // }
+        while (n > 1) {
 
-            if(array[n].compareTo(array[n/2]) >= 0) {
+            if (array[n].compareTo(array[n / 2]) >= 0) {
                 n = -1;
-            }
-            else {
+            } else {
                 return false;
             }
         }
@@ -72,63 +71,67 @@ class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String type = scan.nextLine();
-        switch(type){
-            case "String":
+        switch (type) {
+        case "String":
 
-                int testcase = Integer.parseInt(scan.nextLine());
-                for(int i = 0; i< testcase; i++){
-                    String[] input = scan.nextLine().split(",");
-                    MinPQ<String> obj = new MinPQ<String>();
-                   System.out.println(obj.minHeapchecking(input));
-                }
-
-            break;
-            case "Integer" :
-
-                int testcase1 = Integer.parseInt(scan.nextLine());
-                for(int i = 0; i< testcase1; i++){
-                    String[] input = scan.nextLine().split(",");
-                    Integer[] input1 = new Integer[input.length];
-                    for(int j = 0;j<input.length;j++){
-                        input1[j] = Integer.parseInt(input[j]);
-
-                    }
-                    MinPQ<Integer> obj = new MinPQ<Integer>();
-                   System.out.println(obj.minHeapchecking(input1));
-
-                }
+            int testcase = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < testcase; i++) {
+                String[] input = scan.nextLine().split(",");
+                MinPQ<String> obj = new MinPQ<String>();
+                System.out.println(obj.minHeapchecking(input));
+            }
 
             break;
+        case "Integer" :
 
-            case "Double" :
-                 int testcase2 = Integer.parseInt(scan.nextLine());
-                for(int i = 0; i< testcase2; i++){
-                    String[] input = scan.nextLine().split(",");
-                    Double[] input1 = new Double[input.length];
-                    for(int j = 0;j<input.length;j++){
-                        input1[j] = Double.parseDouble(input[j]);
-
-                    }
-                    MinPQ<Double> obj = new MinPQ<Double>();
-                   System.out.println(obj.minHeapchecking(input1));
+            int testcase1 = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < testcase1; i++) {
+                String[] input = scan.nextLine().split(",");
+                Integer[] input1 = new Integer[input.length];
+                for (int j = 0; j < input.length; j++) {
+                    input1[j] = Integer.parseInt(input[j]);
 
                 }
+                MinPQ<Integer> obj = new MinPQ<Integer>();
+                System.out.println(obj.minHeapchecking(input1));
+
+            }
 
             break;
-            case "Float" :
 
-                int testcase3 = Integer.parseInt(scan.nextLine());
-                for(int i = 0; i< testcase3; i++){
-                    String[] input = scan.nextLine().split(",");
-                    Float[] input1 = new Float[input.length];
-                    for(int j = 0;j<input.length;j++){
-                        input1[j] = Float.parseFloat(input[j]);
-
-                    }
-                    MinPQ<Float> obj = new MinPQ<Float>();
-                   System.out.println(obj.minHeapchecking(input1));
+        case "Double" :
+            int testcase2 = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < testcase2; i++) {
+                String[] input = scan.nextLine().split(",");
+                Double[] input1 = new Double[input.length];
+                for (int j = 0; j < input.length; j++) {
+                    input1[j] = Double.parseDouble(input[j]);
 
                 }
+                MinPQ<Double> obj = new MinPQ<Double>();
+                System.out.println(obj.minHeapchecking(input1));
+
+            }
+
+            break;
+        case "Float" :
+
+            int testcase3 = Integer.parseInt(scan.nextLine());
+            for (int i = 0; i < testcase3; i++) {
+                String[] input = scan.nextLine().split(",");
+                Float[] input1 = new Float[input.length];
+                if (input1.length == 0) {
+                    System.out.println("false");
+                    break;
+                }
+                for (int j = 0; j < input.length; j++) {
+                    input1[j] = Float.parseFloat(input[j]);
+
+                }
+                MinPQ<Float> obj = new MinPQ<Float>();
+                System.out.println(obj.minHeapchecking(input1));
+
+            }
 
         }
     }
