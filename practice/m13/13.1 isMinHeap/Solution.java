35a46a2dc23236b69df0a225015d5a6b487694d3
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-08 19:16:37 +0530

    "ads1_m13_a1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m13/13.1 isMinHeap/Solution.java
+++ practice/m13/13.1 isMinHeap/Solution.java
@@ -3,52 +3,68 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 
-class MinPQ<Key> {
-    private  Key[] pq;
-    private  int n;
-    Comparator<Key> comparator;
-    MinPQ(Key[] array, int size) {
-        pq = array;
-        n = size;
+class MinPQ<Key extends Comparable<Key>> {
+    // private  Key[] pq;
+    // private  int n;
+    // Comparator<Key> comparator;
+    MinPQ() {
+
    }
 
 
 
-    boolean isMinHeap() {
-        return isMinHeap(1);
-    }
+    // boolean isMinHeap() {
+    //     return isMinHeap(1);
+    // }
 
-    // is subtree of pq[1..n] rooted at k a min heap?
-    private boolean isMinHeap(int k) {
-        if (k > n) return true;
-        int left = 2*k;
-        int right = 2*k + 1;
-        if (left  <= n && greater(k, left))  return false;
-        if (right <= n && greater(k, right)) return false;
-        return isMinHeap(left) && isMinHeap(right);
-    }
+    // // is subtree of pq[1..n] rooted at k a min heap?
+    // private boolean isMinHeap(int k) {
+    //     if (k > n) return true;
+    //     int left = 2*k;
+    //     int right = 2*k + 1;
+    //     if (left  <= n && greater(k, left))  return false;
+    //     if (right <= n && greater(k, right)) return false;
+    //     return isMinHeap(left) && isMinHeap(right);
+    // }
 
-     private boolean greater(int i, int j) {
-        if (comparator == null) {
-            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+    //  private boolean greater(int i, int j) {
+    //     if (comparator == null) {
+    //         return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+    //     }
+    //     else {
+    //         return comparator.compare(pq[i], pq[j]) > 0;
+    //     }
+    // }
+    public boolean minHeapchecking(Key[] array) {
+        int n = array.length-1;
+        while(n>1) {
+            if(array[n].compareTo(array[n/2]) >= 0) {
+                n = -1;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return comparator.compare(pq[i], pq[j]) > 0;
-        }
+        return true;
     }
-
-
-
-
-
 
 }
 
-
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * Constructs the object.
+     */
     Solution() {
 
     }
+    /**
+     * { main method }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String type = scan.nextLine();
@@ -58,8 +74,8 @@ class Solution {
                 int testcase = Integer.parseInt(scan.nextLine());
                 for(int i = 0; i< testcase; i++){
                     String[] input = scan.nextLine().split(",");
-                    MinPQ<String> obj = new MinPQ<String>(input,input.length);
-                   System.out.println(obj.isMinHeap());
+                    MinPQ<String> obj = new MinPQ<String>();
+                   System.out.println(obj.minHeapchecking(input));
                 }
 
             break;
@@ -73,8 +89,8 @@ class Solution {
                         input1[j] = Integer.parseInt(input[j]);
 
                     }
-                    MinPQ<Integer> obj = new MinPQ<Integer>(input1,input.length);
-                   System.out.println(obj.isMinHeap());
+                    MinPQ<Integer> obj = new MinPQ<Integer>();
+                   System.out.println(obj.minHeapchecking(input1));
 
                 }
 
