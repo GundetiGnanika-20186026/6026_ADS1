f625f488294e638a1fac1920d0fd54c9a6c06bc5
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-10 00:09:59 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/m14/14.1 Dynamic Median Finding/Solution.java
+++ practice/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,103 +1,60 @@
+
 import java.util.Scanner;
-import java.util.Arrays;
-class Maxpq {
-	int[] array;
-	int size;
-	Maxpq(int length) {
-		array = new int[length+1];
-		size = 0;
-	}
-//////////////////////////////////////////////
-    public void insert(int value) {
-		array[++size] = value;
-        sort(array);
-		// System.out.println(Arrays.toString(array));
-		// System.out.println(size);
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
 
+        MinPQ<Float> minpq = new MinPQ<Float>(n);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
 
-	    System.out.println(median());
-	}
+        Float median = 0.0f;
+        for (int i = 0; i < n; i++) {
+            Float val = scan.nextFloat();
+            if (val > median) {
+                minpq.insert(val);
+            } else {
+                maxpq.insert(val);
+            }
 
-/////////////////////////////////
+            if (minpq.size() - maxpq.size() > 1) {
+                maxpq.insert(minpq.delMin());
 
+            }
+            if (maxpq.size() - minpq.size() > 1) {
+                minpq.insert(maxpq.delMax());
 
-	public double median() {
-        // int[] array1 = new int[size];
-        // int m = 0;
-        // for(int i =1 ; i<size; i++) {
-        //     array1[m] = array[i];
-        //     m++;
-        // }
+            }
 
-        // System.out.println(Arrays.toString(array1));
-		if (size == 1) {
-			return array[1];
-		}
-		if (size % 2 == 0){
-			double b = array[size/2]+array[(size/2)+1];
-			b = b/2;
-			// System.out.println((array[size/2]+"   "+array[(size/2)+1]));
-			return b;
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max()) / 2;
+                System.out.println(median);
+            }
 
-		}
-		else {
-			return array[(size+1)/2];
-		}
-		// return 1;
+            if (maxpq.size() > minpq.size()) {
+                median = maxpq.max();
+                System.out.println(median);
+            }
 
-	}
-////////////////////////////////////
-	public void sort(int[] array) {
-		int n = size;
-		for (int k = n / 2; k >= 1; k--) {
-			sink(array, k, n);
-		}
-		while (n > 1) {
-			exch(array, 1, n);
-			sink(array, 1, --n);
-		}
-	}
-/////////////////////////////
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
 
-	public void sink(int[] array1, int k, int n) {
-		while (2 * k <= n) {
-			int j = 2 * k;
-			if (j < n && less(j, j + 1)) j++;
-			if (!less(k, j)) break;
-			exch(array, k, j);
-			k = j;
-		}
-	}
-///////////////////////////////////////
-	public boolean less(int i, int j) {
-		return array[i]-array[j] < 0;
-	}
-////////////////////////////////////
+        }
 
-	public void exch(int[] array, int i, int j) {
-		int t = array[i];
-		array[i] = array[j];
-		array[j] = t;
-	}
-/////////////////////////////////////
-
-
-
-}
-class Solution {
-	Solution() {
-
-	}
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		int num = Integer.parseInt(scan.nextLine());
-		Maxpq obj = new Maxpq(num);
-		for (int i = 0; i < num; i++) {
-             int element = Integer.parseInt(scan.nextLine());
-             obj.insert(element);
-
-
-		}
-	}
+    }
 }
