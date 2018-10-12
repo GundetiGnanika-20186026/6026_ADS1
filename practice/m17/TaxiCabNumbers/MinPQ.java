542838955e4185fbff33b3afba0480ce6ea63270
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-12 15:53:48 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m17/TaxiCabNumbers/MinPQ.java
+++ practice/m17/TaxiCabNumbers/MinPQ.java
@@ -2,7 +2,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MinPQ<Key> implements Iterable<Key> {
+public class MinPQ<Key> {
     private Key[] pq;                    // store items at indices 1 to n
     private int n;                       // number of items on priority queue
     private Comparator<Key> comparator;  // optional comparator
@@ -37,7 +37,7 @@ public class MinPQ<Key> implements Iterable<Key> {
             pq[i + 1] = keys[i];
         for (int k = n / 2; k >= 1; k--)
             sink(k);
-        assert isMinHeap();
+        //assert isMinHeap();
     }
 
 
@@ -52,7 +52,7 @@ public class MinPQ<Key> implements Iterable<Key> {
 
 
     public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        //if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
         return pq[1];
     }
 
@@ -73,17 +73,17 @@ public class MinPQ<Key> implements Iterable<Key> {
 
         pq[++n] = x;
         swim(n);
-        assert isMinHeap();
+        //assert isMinHeap();
     }
 
     public Key delMin() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        //if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
         Key min = pq[1];
         exch(1, n--);
         sink(1);
         pq[n + 1] = null;
         if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
-        assert isMinHeap();
+        //assert isMinHeap();
         return min;
     }
 
@@ -122,50 +122,35 @@ public class MinPQ<Key> implements Iterable<Key> {
     }
 
     // is pq[1..N] a min heap?
-    private boolean isMinHeap() {
-        return isMinHeap(1);
-    }
+    // private boolean isMinHeap() {
+    //     return isMinHeap(1);
+    // }
 
     // is subtree of pq[1..n] rooted at k a min heap?
-    private boolean isMinHeap(int k) {
-        if (k > n) return true;
-        int left = 2 * k;
-        int right = 2 * k + 1;
-        if (left  <= n && greater(k, left))  return false;
-        if (right <= n && greater(k, right)) return false;
-        return isMinHeap(left) && isMinHeap(right);
+    // private boolean isMinHeap(int k) {
+    //     if (k > n) return true;
+    //     int left = 2 * k;
+    //     int right = 2 * k + 1;
+    //     if (left  <= n && greater(k, left))  return false;
+    //     if (right <= n && greater(k, right)) return false;
+    //     return isMinHeap(left) && isMinHeap(right);
+    // }
+
+
+
+
+
+        //public boolean hasNext()  { return !copy.isEmpty();                     }
+        //public void remove()      { throw new UnsupportedOperationException();  }
+
+        // public Key next() {
+        //     //if (!hasNext()) throw new NoSuchElementException();
+        //     return copy.delMin();
+        // }
     }
 
 
 
-    public Iterator<Key> iterator() {
-        return new HeapIterator();
-    }
 
-    private class HeapIterator implements Iterator<Key> {
-        // create a new pq
-        private MinPQ<Key> copy;
-
-        // add all items to copy of heap
-        // takes linear time since already in heap order so no keys move
-        public HeapIterator() {
-            if (comparator == null) copy = new MinPQ<Key>(size());
-            else                    copy = new MinPQ<Key>(size(), comparator);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i]);
-        }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Key next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            return copy.delMin();
-        }
-    }
-
-
-
-}
 
 
