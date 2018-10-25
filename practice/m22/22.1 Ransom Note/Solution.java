31160fb5fe5cd5a2970dacbdfcacb1418cea3799
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-25 20:36:31 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m22/22.1 Ransom Note/Solution.java
+++ practice/m22/22.1 Ransom Note/Solution.java
@@ -1,19 +1,42 @@
 import java.util.Scanner;
-
+/**
+ * Class for hash st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
 
 class HashST<Key, Value> {
+    /**
+     * capacity of hash table.
+     */
 
     private static final int INIT_CAPACITY = 4;
-
+    /**
+     * size.
+     */
     private int n;
-
+    /**
+     * hashtable size.
+     */
     private int m;
-
+    /**
+     * hashtable array.
+     */
     private SequentialSearchST<Key, Value>[] st;
 
+    /**
+     * Constructs the object.
+     */
     public HashST() {
         this(INIT_CAPACITY);
     }
+
+    /**
+     * Constructs the object.
+     *
+     * @param      m     { capacity of hash table }
+     */
 
     public HashST(final int m) {
         this.m = m;
@@ -22,23 +45,59 @@ class HashST<Key, Value> {
             st[i] = new SequentialSearchST<Key, Value>();
     }
 
+    /**
+     * generates hash which acts as index.
+     * its complexity is O(N).
+     *
+     * @param      key   The key
+     *
+     * @return     { value where the node to be placed }
+     */
     private int hash(final Key key) {
         return (key.hashCode() & 0x7fffffff) % m;
     }
-
+    /**
+     * returns size.
+     * its complexity is O(1).
+     *
+     * @return     { size }
+     */
     public int size() {
         return n;
     }
 
+    /**
+     * checks wether the key is present.
+     * its complexity is O(N). as we are using hash.
+     *
+     * @param      key   The key
+     *
+     * @return     { true or false }
+     */
     public boolean contains(final Key key) {
         return get(key) != null;
     }
 
+    /**
+     * returns the value of keep.
+     * its complexity is O(N). as we are using hash.
+     *
+     * @param      key   The key
+     *
+     * @return     { value }
+     */
     public Value get(final Key key) {
         int i = hash(key);
         return st[i].get(key);
     }
 
+    /**
+     * inserts the node.
+     * its complexity is O(N). as we are using hash.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
     public void put(final Key key, final Value val) {
         int i = hash(key);
         if (!st[i].contains(key)) n++;
@@ -46,15 +105,46 @@ class HashST<Key, Value> {
     }
 }
 
+/**
+ * Class for sequential search st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
+
 class SequentialSearchST<Key, Value> {
+    /**
+     * size.
+     */
     private int n;
+    /**
+     * first node.
+     */
     private Node first;
 
-
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * key.
+         */
         private Key key;
+        /**
+         * value.
+         */
         private Value val;
+        /**
+         * nextnode pointer.
+         */
         private Node next;
+        /**
+         * Constructs the object.
+         *
+         * @param      key   The key
+         * @param      val   The value
+         * @param      next  The next
+         */
 
         public Node(Key key, Value val, Node next)  {
             this.key  = key;
@@ -63,25 +153,54 @@ class SequentialSearchST<Key, Value> {
         }
     }
 
+    /**
+     * Constructs the object.
+     */
+
     public SequentialSearchST() {
     }
+
+    /**
+     * returns the size.
+     * its complexity is O(1).
+     *
+     * @return     { size }
+     */
 
     public int size() {
         return n;
     }
 
+    // /**
+    //  * Determines if empty.
+    //  *
+    //  * @return     True if empty, False otherwise.
+    //  */
+    // public boolean isEmpty() {
+    //     return size() == 0;
+    // }
 
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-
-    public boolean contains(Key key) {
+    /**
+     * checks the presence of the key.
+     * its complexity is O(N) as we use get method.
+     *
+     * @param      key   The key
+     *
+     * @return     { true if present and false if not }
+     */
+    public boolean contains(final Key key) {
 
         return get(key) != null;
     }
 
-     public void put(Key key, Value val) {
+    /**
+     * will insert the node.
+     * its complexity is O(1).as we are inserting the node at the beginning.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final Key key,final Value val) {
 
         for (Node x = first; x != null; x = x.next) {
             if (key.equals(x.key)) {
@@ -93,7 +212,15 @@ class SequentialSearchST<Key, Value> {
         n++;
     }
 
-    public Value get(Key key) {
+    /**
+     * gets the value of given key.
+     * its complexity is O(N).
+     *
+     * @param      key   The key
+     *
+     * @return     { value }
+     */
+    public Value get(final Key key) {
 
         for (Node x = first; x != null; x = x.next) {
             if (key.equals(x.key))
@@ -106,8 +233,24 @@ class SequentialSearchST<Key, Value> {
 
 }
 
+
+/**
+ * Class for solution.
+ */
+
 class Solution {
-    public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    Solution(){
+
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] inp = sc.nextLine().split(" ");
         int m = Integer.parseInt(inp[0]);
